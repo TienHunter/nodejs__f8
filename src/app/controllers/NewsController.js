62bb47f9fbf7130d650ec3589f356787e84ef971
TienHunter <tienkbtnhp@gmail.com>
2021-10-29 23:01:46 +0700

    Router & Controller

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,14 @@
+
+class NewsController {
+   // [GET] /new 
+   index(req, res) {
+      res.render('news')
+   }
+
+   // [GET] /new/:slug 
+   show(req, res) {
+      res.send('News detail')
+   }
+
+}
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000
+
+const route = require('./routes')
 //static files in express
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+   extended: true
+}))
+app.use(express.json())
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // template engine
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 app.set('views', path.join(__dirname, 'resources/views'));
-console.log(path.join(__dirname, 'resources/views'));
-app.get('/news', (req, res) => {
-   res.render('news');
-})
+
+// routes init
+route(app);
 
 app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}`)
-})
+});
